332da29fb43c97cbf7c5d7085a9484c325e18dcf
merdandt <meinnps@gmail.com>
2025-06-17 12:31:16 -0600

    Human in the loop does not work

--- sdr/sdr/sub_agents/outreach_email_agent/sub_agents/website_creator/prompt_prepare_agent.py
+++ sdr/sdr/sub_agents/outreach_email_agent/sub_agents/website_creator/prompt_prepare_agent.py
@@ -1,7 +1,8 @@
 from google.adk.agents.llm_agent import LlmAgent
 from .website_creator_prompts import PROMPT_PREPARE_PROMPT
 
-prepare_request = LlmAgent(
+prepare_prompt = LlmAgent(
     name="PrepareApproval",
-    instruction=PROMPT_PREPARE_PROMPT
+    instruction=PROMPT_PREPARE_PROMPT,
+    output_key="website_creation_prompt",
 )
